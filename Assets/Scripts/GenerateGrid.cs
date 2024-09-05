3cedc83061385f920325bbf1c73be4a7e7228df3
Bodes1 <b_h097@student.usc.edu.au>
2024-09-05 20:57:50 +1000

    Added Perlin Noise to the code to give the grid depth

--- Assets/Scripts/GenerateGrid.cs
+++ Assets/Scripts/GenerateGrid.cs
@@ -7,10 +7,13 @@ public class GenerateGrid : MonoBehaviour
     public GameObject blockGameObject;
 
     // Defind world size
-    private int worldSizeX = 10;
-    private int worldSizeZ = 10;
+    private int worldSizeX = 40;
+    private int worldSizeZ = 40;
 
-    private int gridOffset = 2;
+    private int noiseHeight = 5;
+
+    // Space between cubes
+    private float gridOffset = 1.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +23,7 @@ public class GenerateGrid : MonoBehaviour
         {
             for (int z = 0; z < worldSizeZ; z++)
             {
-                Vector3 pos = new Vector3 (x * gridOffset, 0, z * gridOffset);
+                Vector3 pos = new Vector3 (x * gridOffset, generateNoise(x, z, 8f) * noiseHeight, z * gridOffset);
 
                 GameObject block = Instantiate (blockGameObject, pos, Quaternion.identity) as GameObject;
 
@@ -29,9 +32,12 @@ public class GenerateGrid : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Generating and getting noise values
+    private float generateNoise(int x, int z, float detailScale)
     {
-        
+        float xNoise = (x + this.transform.position.x) / detailScale;
+        float zNoise = (z + this.transform.position.y) / detailScale;
+
+        return Mathf.PerlinNoise(xNoise, zNoise);
     }
 }
