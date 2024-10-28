dd83ccc29c77f61726b50bcd05f6cf61a7627ae6
Bodes1 <b_h097@student.usc.edu.au>
2024-10-28 17:14:58 +1000

    Random tree height and ores

--- Assets/Scripts/CubeProceduralGeneration.cs
+++ Assets/Scripts/CubeProceduralGeneration.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
+using UnityEditor.Overlays;
 using UnityEngine;
 
-public class TestGeneration : MonoBehaviour
+public class CubeProceduralGeneration : MonoBehaviour
 {
     // Public material fields to assign in the Unity Editor
     public Material waterMaterial;
@@ -11,6 +13,8 @@ public class TestGeneration : MonoBehaviour
     public Material stoneMaterial;
     public Material woodMaterial;
     public Material leafMaterial;
+    public Material coalMaterial;
+    public Material ironMaterial;
 
     // Chunk size and settings
     public Int32 chunkSize = 16;
@@ -27,6 +31,11 @@ public class TestGeneration : MonoBehaviour
     // Tree generation
     [Range(0f, 1f)]
     public float treeSpawnChance = 0.1f;
+
+    // Ore generation
+    [Range(0f, 1f)]
+    public float oreSpawnChance = 0.1f;
+    public Int32 maxOreClusterSize = 3;
 
     // Seed for random generation
     public Int32 seed;
@@ -62,6 +71,10 @@ public class TestGeneration : MonoBehaviour
         List<CombineInstance> stoneCombineInstances = new List<CombineInstance>();
         List<CombineInstance> woodCombineInstances = new List<CombineInstance>();
         List<CombineInstance> leafCombineInstances = new List<CombineInstance>();
+        List<CombineInstance> coalCombineInstances = new List<CombineInstance>();
+        List<CombineInstance> ironCombineInstances = new List<CombineInstance>();
+
+        HashSet<Vector3> placedPositions = new HashSet<Vector3>(); // Track placed blocks
 
         for (Int32 x = 0; x < chunkSize; x++)
         {
@@ -78,18 +91,33 @@ public class TestGeneration : MonoBehaviour
                 // Generate terrain up to the calculated terrain height
                 for (Int32 y = 0; y < worldHeight; y++)
                 {
+                    Vector3 blockPosition = new Vector3(worldX * cubeSize, y * cubeSize, worldZ * cubeSize);
+
                     if (y <= terrainHeight)
                     {
                         if (y < waterLevel)
-                            AddCubeToMesh(new Vector3(worldX * cubeSize, y * cubeSize, worldZ * cubeSize), waterCombineInstances);
+                        {
+                            AddCubeToMesh(blockPosition, waterCombineInstances);
+                        }
                         else if (y < stoneHeightThreshold)
-                            AddCubeToMesh(new Vector3(worldX * cubeSize, y * cubeSize, worldZ * cubeSize), grassCombineInstances);
+                        {
+                            AddCubeToMesh(blockPosition, grassCombineInstances);
+                        }
                         else
-                            AddCubeToMesh(new Vector3(worldX * cubeSize, y * cubeSize, worldZ * cubeSize), stoneCombineInstances);
+                        {
+                            AddCubeToMesh(blockPosition, stoneCombineInstances);
+                            placedPositions.Add(blockPosition); // Track stone position
+
+                            // Check for ore spawning on stone blocks
+                            if (UnityEngine.Random.value < oreSpawnChance)
+                            {
+                                PlaceOreCluster(worldX, terrainHeight + 1, worldZ, coalCombineInstances, ironCombineInstances, placedPositions);
+                            }
+                        }
                     }
                     else if (y < waterLevel)
                     {
-                        AddCubeToMesh(new Vector3(worldX * cubeSize, y * cubeSize, worldZ * cubeSize), waterCombineInstances);
+                        AddCubeToMesh(blockPosition, waterCombineInstances);
                     }
                 }
 
@@ -107,6 +135,8 @@ public class TestGeneration : MonoBehaviour
         CreateCombinedMesh(chunkObject, stoneCombineInstances, stoneMaterial);
         CreateCombinedMesh(chunkObject, woodCombineInstances, woodMaterial);
         CreateCombinedMesh(chunkObject, leafCombineInstances, leafMaterial);
+        CreateCombinedMesh(chunkObject, coalCombineInstances, coalMaterial);
+        CreateCombinedMesh(chunkObject, ironCombineInstances, ironMaterial);
     }
 
     void AddCubeToMesh(Vector3 position, List<CombineInstance> combineInstances)
@@ -149,14 +179,51 @@ public class TestGeneration : MonoBehaviour
 
     void PlaceTree(Int32 x, Int32 y, Int32 z, List<CombineInstance> woodCombineInstances, List<CombineInstance> leafCombineInstances)
     {
+        Int32 woodPlaced = UnityEngine.Random.Range(2, 5);
+
         // Place 3 cubes as the tree trunk
-        for (Int32 i = 0; i < 3; i++)
+        for (Int32 i = 0; i < woodPlaced; i++)
         {
             AddCubeToMesh(new Vector3(x * cubeSize, (y + i) * cubeSize, z * cubeSize), woodCombineInstances);
         }
 
         // Add a green "leaf" cube at the top
-        AddCubeToMesh(new Vector3(x * cubeSize, (y + 3) * cubeSize, z * cubeSize), leafCombineInstances);
+        AddCubeToMesh(new Vector3(x * cubeSize, (y + woodPlaced) * cubeSize, z * cubeSize), leafCombineInstances);
+    }
+
+    void PlaceOreCluster(Int32 x, Int32 y, Int32 z, List<CombineInstance> coalCombineInstances, List<CombineInstance> ironCombineInstances, HashSet<Vector3> placedPositions)
+    {
+        Int32 clusterSize = UnityEngine.Random.Range(1, maxOreClusterSize + 1);
+
+        for (Int32 i = 0; i < clusterSize; i++)
+        {
+            // Random offset for clustered ores
+            Int32 offsetX = UnityEngine.Random.Range(-1, 2);
+            Int32 offsetY = UnityEngine.Random.Range(-1, 2);
+            Int32 offsetZ = UnityEngine.Random.Range(-1, 2);
+
+            Vector3 orePosition = new Vector3((x + offsetX) * cubeSize, (y + offsetY) * cubeSize, (z + offsetZ) * cubeSize);
+
+            // Check if this position is already used and if there is a stone block below it
+            Vector3 belowPosition = new Vector3(orePosition.x, orePosition.y - cubeSize, orePosition.z);
+
+            if (!placedPositions.Contains(orePosition) && placedPositions.Contains(belowPosition))
+            {
+
+                // Decide if this ore block should be coal or iron
+                if (UnityEngine.Random.value < 0.9f) // 90% chance for coal
+                {
+                    AddCubeToMesh(orePosition, coalCombineInstances);
+                }
+                else // 10% chance for iron
+                {
+                    AddCubeToMesh(orePosition, ironCombineInstances);
+                }
+
+                // Mark this position as occupied to avoid overlap
+                placedPositions.Add(orePosition);
+            }
+        }
     }
 
     // Draw chunk outline
